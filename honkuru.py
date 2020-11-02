4ba8ea6398202594e44958f3f2b3b5e3cfababfa
Dakes <dakes@vivaldi.net>
2020-11-02 22:16:57 +0100

    Removed Legacy code

--- honkuru.py
+++ honkuru.py
@@ -38,7 +38,6 @@ class Honkuru(object):
         # TODO: check port and IP for validity
         self.server_port = int(config["server"]["port"])
         self.server_ip = config["server"]["ip"]
-        self.client_ports = self.parse_range_list(config["client"]["ports"])
 
         # test if a server is available on server_port
         server_exist = self.check_server()
@@ -90,31 +89,6 @@ class Honkuru(object):
         else:
             c = client.Client(self.server_ip, self.server_port, None, self.verbose)
             c.main()
-
-    def parse_range_list(self, rgstr):
-        """
-        Parses a list of integers in a string, in the form: "7951 - 7999, 8001, 8012" into an integer array
-        Copied from https://gist.github.com/kgaughan/2491663
-        :param rgstr: String of integers separated by "," of "-"
-        :return: int Array
-        """
-        def parse_range(rg):
-            if len(rg) == 0:
-                return []
-            parts = re.split(r'[:-]', rg)
-            if len(parts) > 2:
-                raise ValueError("Invalid range: {}".format(rg))
-            try:
-                return range(int(parts[0]), int(parts[-1]) + 1)
-            except ValueError:
-                if len(parts) == 1:
-                    print("Strings are not allowed. ", "Please remove:", parts)
-                    exit(1)
-                else:
-                    raise ValueError("Non-integer range: {}".format(rg))
-
-        rg = map(parse_range, re.split("\s*[,;]\s*", rgstr))
-        return list(set(chain.from_iterable(rg)))
 
 
 if __name__ == "__main__":
